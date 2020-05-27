1fd0f8ff45e0ba00bf15b352aac62c89a471d15c
lucasrroscoeoliveira <lucasrloliveira@gmail.com>
2020-05-26 21:46:45 -0300

    trying to access API from frontend dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://100.24.105.5:3000/api',
-  baseUrl: 'http://100.24.105.5:3000',
+  api: 'http://ec2-52-91-145-127.compute-1.amazonaws.com/api',
+  baseUrl: 'http://ec2-52-91-145-127.compute-1.amazonaws.com',
   s3_public: 'https://vies-images.s3.amazonaws.com/Images'
 };
