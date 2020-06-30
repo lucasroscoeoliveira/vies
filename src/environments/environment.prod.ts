e42f24208f04f15d688469619fa77d4bbb1fb4d3
lucasrroscoeoliveira <lucasrloliveira@gmail.com>
2020-06-30 02:58:02 -0300

    adjust api route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://apidev.viesdesign.com.br:3000/api',
-  baseUrl: 'https://apidev.viesdesign.com.br:3000/',
+  api: 'https://apidev.viesdesign.com.br/api',
+  baseUrl: 'https://apidev.viesdesign.com.br/',
   s3_public: 'https://vies-images.s3.amazonaws.com/Images'
 };
