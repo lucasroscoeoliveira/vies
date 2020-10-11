fa3f812667366d5b49aecde457af4b3cb8ba9c45
lucasrroscoeoliveira <lucasrloliveira@gmail.com>
2020-10-11 11:54:11 -0300

    adding image to display link

--- src/app/components-controllers/Social.ts
+++ src/app/components-controllers/Social.ts
@@ -1,31 +1,43 @@
 import { Injectable, OnInit, Input } from '@angular/core';
+import { Menu } from './Menu';
 
 @Injectable()
 export class Social {
 
   static instance: Social;
 
-  constructor(){
+  constructor() {
     return Social.instance = Social.instance || this;
   }
 
-  goToFacebook() {
-      window.open('https://www.facebook.com/viescasa/');
+  goToFacebook(type: string) {
+    debugger;
+    switch (type) {
+      case 'house':
+        window.open('https://www.facebook.com/viescasa/');
+        break;
+      default:
+        window.open('https://www.facebook.com/viesdesign/');
+        break;
+    }
   }
 
-  goToInstagram() {
-    window.open('https://instagram.com/viescasa?igshid=9zzhn2xo4fq2');
+  goToInstagram(type: string) {
+    switch (type) {
+      case 'house':
+        window.open('https://instagram.com/viescasa?igshid=9zzhn2xo4fq2');
+        break;
+      default:
+        window.open('https://instagram.com/viesdesign?igshid=1mxx2r0u43oai');
+        break;
+    }
   }
 
-  goToPinterest() {
+  goToPinterest(type: string) {
     window.open('https://br.pinterest.com/designvies/');
   }
 
-  goToYoutube() {
+  goToYoutube(type: string) {
     window.open('https://www.youtube.com/user/viesdesign');
   }
-
-  // goToLinkedin() {
-  //   window.open('https://www.linkedin.com/company/vi%C3%A9s-design---ambientes-para-moda/about/?viewAsMember=true');
-  // }
 }
